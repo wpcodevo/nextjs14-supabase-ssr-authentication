73fd4843f7eab18870659039f94143895f71d72b
wpcodevo <ziddah.edem@gmail.com>
2024-02-01 11:01:32 +0000

    updated

--- app/_actions.ts
+++ app/_actions.ts
@@ -1,13 +1,22 @@
-'use server';
+"use server";
 
-import createSupabaseServerClient from '@/lib/supabase/server';
-import { CreateUserInput, LoginUserInput } from '@/lib/user-schema';
+import createSupabaseServerClient from "@/lib/supabase/server";
+import { CreateUserInput, LoginUserInput } from "@/lib/user-schema";
 
-export async function signUpWithEmailAndPassword(data: CreateUserInput) {
+export async function signUpWithEmailAndPassword({
+  data,
+  emailRedirectTo,
+}: {
+  data: CreateUserInput;
+  emailRedirectTo?: string;
+}) {
   const supabase = await createSupabaseServerClient();
   const result = await supabase.auth.signUp({
     email: data.email,
     password: data.password,
+    options: {
+      emailRedirectTo,
+    },
   });
   return JSON.stringify(result);
 }
